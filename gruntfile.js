69433356f30a114b4f494fba3dd5cc1a46127bd8
Elizabeth Reiher <tonomoshia@gmail.com>
2017-04-06 22:24:29 -0400

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    //Project configuration
+    grunt.initConfig({
+        /*
+        *We will configure our tasks here
+        */
+        concat: {
+            release: {
+                src: ['js/values.js', 'js/prompt.js'],
+                dest: 'release/main.js'
+            }
+        },
+        copy: {
+            release: {
+                src: 'manifest.json',
+                dest: 'release/manifest.json'
+            }
+        },
+        jshint: {
+            files: ['js/values.js', 'js/prompt.js']
+        }
+    });
+
+    //Load Grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+
+    //Register tasks
+    grunt.registerTask('default', ['jshint', 'concat', 'copy']);
+};
